0279aef3808b34b68a12f91338305f88678e70ca
Charles Bos <charlesbos1@gmail.com>
2014-12-11 13:34:24 +0000

    added fucntion to find which shop cheapest item came from as this information is lost on merging the dictionarys for cheapest items

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ def lowestPrices(priceList) :
             
     return dict(zip(minKeys, minVals))
 
+def cheapestFrom() :
+    '''
+    A way of finding which shop the lowest price came from as this information
+    is not preserved when merging our lists.
+    No arguments taken.
+    '''
+    if tescoPrices[cheapestItem] != None : return "Tesco"
+    if lowestPriceDic[cheapestItem] != None : return "Sainsbury's"
+
 # Call functions from the shop modules to extract item titles and prices
 tescoPrices = tescoData("http://www.tesco.com/groceries/product/browse/default.aspx?N=4294792641&Ne=4294793660")
 sainsburysPrices = sainsburysData("http://www.sainsburys.co.uk/shop/gb/groceries/drinks/still-water#langId=44&storeId=10151&catalogId=10122&categoryId=12351&parent_category_rn=12192&top_category=12192&pageSize=30&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0")
@@ -33,5 +42,6 @@ print("The cheapest water from Sainsbury's:", cheapestSainsburys)
 
 cheapByShop = dict(list(cheapestTesco.items()) + list(cheapestSainsburys.items()))
 cheapestOverall = lowestPrices(cheapByShop)
+cheapestItem = list(cheapestOverall.keys())[0]
 
-print("\nThe cheapest water overall:", cheapestOverall)
+print("\nThe cheapest water overall:", cheapestOverall, "from", cheapestFrom())
